f80e3ad7342140ca625e77c0b8c5bce7093412b2
John Roers <jroers@gmail.com>
2015-11-18 08:28:04 -0600

    UIColorFromRGB

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -32,6 +32,8 @@
 #define    LOCATIONBAR_HEIGHT 21.0
 #define    FOOTER_HEIGHT ((TOOLBAR_HEIGHT) + (LOCATIONBAR_HEIGHT))
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
 #pragma mark CDVInAppBrowser
 
 @interface CDVInAppBrowser () {
